04ae1c7d69f5f6e5cbd5cb85504efc2a35d273a6
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2020-01-13 12:00:58 +0300

    Final Start Project

--- MultipleSections/Controller/ListViewController.swift
+++ MultipleSections/Controller/ListViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 class ListViewController: UIViewController {
     
     let sections = Bundle.main.decode([MSection].self, from: "model.json")
-    var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,42 +21,6 @@ class ListViewController: UIViewController {
 //                print(chat.friendName)
 //            }
 //        }
-        setupCollectionView()
-    }
-    
-    private func setupCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
-        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        collectionView.backgroundColor = .systemBackground
-        view.addSubview(collectionView)
-        collectionView.backgroundColor = #colorLiteral(red: 0.968627451, green: 0.9725490196, blue: 0.9921568627, alpha: 1)
-        
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellid")
-        
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        
-        let flow = self.collectionView.collectionViewLayout as! UICollectionViewFlowLayout
-        
-        flow.sectionInset = UIEdgeInsets(top: 10, left: 0, bottom: 0, right: 0)
-        
-    }
-}
-
-extension ListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return sections.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return sections[section].items.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellid", for: indexPath)
-        cell.backgroundColor = .red
-        return cell
     }
 }
 
