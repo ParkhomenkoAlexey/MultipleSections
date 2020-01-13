50050888bcf47066c2c3c6c0b17ee467c0263d22
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2020-01-13 12:00:14 +0300

    Create simple flow collectionView

--- MultipleSections/Controller/ListViewController.swift
+++ MultipleSections/Controller/ListViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ListViewController: UIViewController {
     
     let sections = Bundle.main.decode([MSection].self, from: "model.json")
+    var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +22,64 @@ class ListViewController: UIViewController {
 //                print(chat.friendName)
 //            }
 //        }
+        setupCollectionView()
+    }
+    
+    private func setupCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .systemBackground
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = #colorLiteral(red: 0.968627451, green: 0.9725490196, blue: 0.9921568627, alpha: 1)
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellid")
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        let flow = self.collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        
+        flow.sectionInset = UIEdgeInsets(top: 10, left: 0, bottom: 0, right: 0)
+        
     }
 }
+
+extension ListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return sections.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return sections[section].items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellid", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+}
+
+// MARK: - SwiftUI
+import SwiftUI
+struct ListProvider: PreviewProvider {
+    static var previews: some View {
+        ContainterView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainterView: UIViewControllerRepresentable {
+        
+        let listVC = ListViewController()
+        func makeUIViewController(context: UIViewControllerRepresentableContext<ListProvider.ContainterView>) -> ListViewController {
+            return listVC
+        }
+        
+        func updateUIViewController(_ uiViewController: ListProvider.ContainterView.UIViewControllerType, context: UIViewControllerRepresentableContext<ListProvider.ContainterView>) {
+            
+        }
+    }
+}
+
+
 
