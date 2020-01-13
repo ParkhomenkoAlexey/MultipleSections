032c00f5c352499363c63fa1865b3c571ef71130
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2020-01-13 11:34:26 +0300

    Start Project

--- MultipleSections/Controller/ListViewController.swift
+++ MultipleSections/Controller/ListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ListViewController.swift
 //  MultipleSections
 //
 //  Created by Алексей Пархоменко on 13.01.2020.
@@ -8,14 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ListViewController: UIViewController {
+    
+    let sections = Bundle.main.decode([MSection].self, from: "model.json")
 
     override func viewDidLoad() {
         super.viewDidLoad()
     
         view.backgroundColor = .orange
+//        sections.map { (section) in
+//            section.items.map { (chat) in
+//                print(chat.friendName)
+//            }
+//        }
     }
-
-
 }
 
